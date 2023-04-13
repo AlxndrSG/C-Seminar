14e2b30453618acfe4afa4540a2ea2bc28f4c682
Alexander <firefighter-911@rambler.ru>
2023-04-13 23:56:56 +0300

    Решение задачи с целыми числами в промежутке

---
+++ Ex3/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая на вход принимает 
+одно число (N), а на выходе показывает все 
+целые числа в промежутка от -N до N.
+Например:
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> "-2, -1, 0, 1, 2"
+*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int N = int.Parse(userInput);
+
+/*for(int i = -N; i <= N; i++)
+{
+    Console.Write(i + " ");
+}
+*/
+
+int index = -N - 1;
+
+while (index < N)
+{
+    index++;
+
+    Console.Write(index + " ");
+}
+
