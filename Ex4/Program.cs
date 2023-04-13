7b0f2cb8b2f522b0eae2522972d723761b57e3cc
Alexander <firefighter-911@rambler.ru>
2023-04-14 00:06:58 +0300

    Решение задачи с последней цифрой трехзначного числа

---
+++ Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на вход 
+трехзначное число и на выходе показывает последнюю цифру.
+Например:
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+int a3 = userNumber % 10;
+
+Console.WriteLine($"{userNumber} -> {a3}");
+
+
+
+
