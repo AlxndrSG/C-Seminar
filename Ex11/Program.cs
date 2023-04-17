84ce1dde3666172d39a971e9de725a3fa8a79854
Alexander <firefighter-911@rambler.ru>
2023-04-17 23:06:46 +0300

    Решение задачи с нахождением третьей цифры числа

---
+++ Ex11/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая выводит третью 
+//цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+
+Console.Clear();
+int varA;
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out varA))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+if (varA >= 100)
+{
+    while (varA > 999)
+    {
+        varA = varA / 10;
+    }
+    int result = varA % 10;
+    Console.WriteLine($"Третья цифра -> {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
