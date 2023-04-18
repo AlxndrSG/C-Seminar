6e81ed6ce6a1bf26af031f4ff0f866e32b47b7ac
Alexander <firefighter-911@rambler.ru>
2023-04-18 22:42:31 +0300

    Решение задачи на сумму чисел

---
+++ Ex14/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 24. Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А (4 -> (1+2+3+4) -> 10)
+
+Console.Clear();
+
+int num = GetNumberFromUser("Введите целое число A: ", "Ошибка ввода!");
+
+int sumNumbers = GetSumNumbers(num);
+
+PrintReport(num, sumNumbers);
+
+///////////////////////////////////
+
+void PrintReport(int num, int number)
+{
+    Console.WriteLine($"{num} -> {number}");
+}
+
+//Console.WriteLine($"{num} -> {sumNumbers}");
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа int.
+// В случае ошибки выводит в консоль сообщение errorMessage
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+// Возвращает сумму чисел от 1 до number
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number;
+        number--;
+    }
+    return sum;
+}
+
