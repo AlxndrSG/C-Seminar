7332bd5492be34e752f3c161121f7f82b3f44918
Alexander <firefighter-911@rambler.ru>
2023-04-18 19:59:14 +0300

    Решение задачи с четверти

---
+++ Ex13/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+int number = GetUserInput("Введите номер четверти: ");
+
+PrinCoordsByQuarter(number);
+
+static int GetUserInput(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+static void PrinCoordsByQuarter(int number)
+{
+    if (number == 1)
+    {
+        Console.WriteLine("X > 0, Y > 0");
+    }
+    else if (number == 2)
+    {
+        Console.WriteLine("X < 0, Y > 0");
+    }
+    else if (number == 3)
+    {
+        Console.WriteLine("X < 0, Y < 0");
+    }
+    else if (number == 4)
+    {
+        Console.WriteLine("X > 0, Y < 0");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка ввода данных");
+    }
+}
