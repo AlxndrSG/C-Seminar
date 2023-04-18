800e2029a1b35312f7a0e97b261a3f35467c28cc
Alexander <firefighter-911@rambler.ru>
2023-04-18 22:43:56 +0300

    Решение задачи на колличество цифр в числе

---
+++ Ex15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int num = GetNumberFromUser("Введите число A: ", "Ошибка ввода!");
+int numDigits = GetNumDigits(num);
+Console.WriteLine($"{num} -> {numDigits}");
+
+int GetNumDigits(int number)
+{
+    int result = 0;
+        while(number > 0)
+        {
+            number/=10;
+            result++;
+        }
+
+    return result;
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
