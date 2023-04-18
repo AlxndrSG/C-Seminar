aecbc8792f48bb2ee3c46e9ecab49b7ec3d3cda6
Alexander <firefighter-911@rambler.ru>
2023-04-18 22:44:38 +0300

    Решение задачи на произведение чисел

---
+++ Ex16/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+int num = GetNumberFromUser("Введите число A: ", "Ошибка ввода!");
+
+int comNumbers = GetComNumbers(num);
+
+PrintReport(num, comNumbers);
+
+void PrintReport(int num, int number)
+{
+    Console.WriteLine($"{num} -> {number}");
+}
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+int GetComNumbers(int number)
+{
+    int com = 1;
+    while (number > 1)
+    {
+        com *= number;
+        number--;
+    }
+    return com;
+}
