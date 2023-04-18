3f7e8a43998f0e9b9c10dc8ead4cceac80c4678e
Alexander <firefighter-911@rambler.ru>
2023-04-18 22:39:21 +0300

    Ошибка ввода

---
+++ Ex7/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int varA;
+while (true)
+{
+    Console.Write("Введите А: ");
+    if (int.TryParse(Console.ReadLine(), out varA))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+Console.WriteLine($"A={varA}");
